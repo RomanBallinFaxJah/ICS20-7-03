d874e03fab2014bd2d6d501e92de7e02dbca1ee4
Roman-Daher <84515748+RomanBallinFaxJah@users.noreply.github.com>
2021-06-09 17:23:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('button').addEventListener('click', agecheck)
+let age
+
+function agecheck () {
+  age = document.getElementById('val1').value
+
+  if (age >= 17) {
+    document.getElementById('answer').innerHTML = 'You are able to see an R rated movie'
+  } else if (age >= 13) {
+    document.getElementById('answer').innerHTML = 'You are able to see an PG-13 rated movie'
+  } else if (age >= 5) {
+    document.getElementById('answer').innerHTML = 'You are able to see PG and G rated movies'
+  } else {
+    document.getElementById('answer').innerHTML = 'You are too young for movies'
+  }
+}
