d844c6c9021fe128c82dbdc2bd3b91378e0d22b5
Roman-Daher <84515748+RomanBallinFaxJah@users.noreply.github.com>
2021-06-09 17:47:21 +0000

    typo

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function agecheck () {
   if (age >= 17) {
     document.getElementById('answer').innerHTML = 'You are able to see an R rated movie'
   } else if (age >= 13) {
-    document.getElementById('answer').innerHTML = 'You are able to see an PG-13 rated movie'
+    document.getElementById('answer').innerHTML = 'You are able to see a PG-13 rated movie'
   } else if (age >= 5) {
     document.getElementById('answer').innerHTML = 'You are able to see PG and G rated movies'
   } else {
